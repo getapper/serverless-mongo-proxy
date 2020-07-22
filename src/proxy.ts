3bc68fdfd8293d6c61e01f09574f70060075227f
Andrea Santu <andreasantu@gmail.com>
2020-07-22 19:05:09 +0200

    Proxy function

---
+++ src/proxy.ts
@@ -0,0 +1,48 @@
+import { Cursor, Db, MongoClient } from 'mongodb';
+import { Context } from 'aws-lambda';
+
+interface ProxyRequest {
+  collection: string
+  operation: string
+  args: any[]
+}
+
+let mongoClient: MongoClient;
+let db: Db;
+
+export default async function(event: ProxyRequest, context: Context) {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  if (!mongoClient) {
+    try {
+      mongoClient = await MongoClient.connect(process.env['MONGO_URI'], {
+        connectTimeoutMS: 10000,
+        socketTimeoutMS: 10000,
+        serverSelectionTimeoutMS: 10000,
+      });
+      db = mongoClient.db(); // @TODO process.env['MONGO_DB_NAME'] ?
+    } catch (e) {
+      return {
+        error: {
+          message: 'Could not connect to the MongoDB server, make sure environment variable MONGO_URI is set.',
+        },
+      };
+    }
+  }
+
+  try {
+    const { collection, operation, args } = event;
+    const result = await db.collection(collection)[operation](...args);
+    if (result instanceof Cursor) {
+      return result.toArray();
+    }
+    return result;
+  } catch (e) {
+    return {
+      error: {
+        name: e.constructor.name,
+        message: e.message,
+      },
+    };
+  }
+}
