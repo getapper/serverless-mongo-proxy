d9ef4da0570585eac49c9c3320261fc6b4721421
Andrea Santu <andreasantu@gmail.com>
2020-07-23 19:08:58 +0200

    plugin hook for serverless package: include proxy code on artifacts (use temporary folder _mongo-proxy)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import path from 'path';
+import { copyFileSync, mkdirSync, rmdirSync } from 'fs';
+
 class ServerlessMongoProxy {
   serverless: any
   options: any
   commands: any
   hooks: any
   proxyFunctionName = '_serverless-mongo-proxy'
+  outputFolder = '_mongo-proxy'
 
   constructor(serverless, options) {
     this.serverless = serverless;
@@ -12,17 +16,33 @@ class ServerlessMongoProxy {
       // @TODO add lifecycle events
     };
     this.hooks = {
-      'before:offline:start:init': this.createProxy.bind(this),
-      'before:offline:start': this.createProxy.bind(this),
-      'after:package:createDeploymentArtifacts': this.createProxy.bind(this),
+      'before:offline:start:init': this.beforeOfflineStart.bind(this),
+      'before:offline:start': this.beforeOfflineStart.bind(this),
+      'after:package:initialize': this.afterPackageInitialize.bind(this),
+      'after:package:createDeploymentArtifacts': this.afterCreateDeploymentArtifacts.bind(this),
     };
   }
 
-  createProxy() {
+  afterPackageInitialize() {
+    mkdirSync(this.outputFolder, { recursive: true });
+    copyFileSync(path.join(__dirname, 'proxy.js'), path.join(this.outputFolder, 'proxy.js'));
+    this.createProxy(this.outputFolder);
+  }
+
+  afterCreateDeploymentArtifacts() {
+    rmdirSync(this.outputFolder, { recursive: true });
+  }
+
+  beforeOfflineStart() {
+    this.createProxy(path.join('node_modules', 'serverless-mongo-proxy', 'dist'));
+  }
+
+  createProxy(handlerFolder: string) {
     const options = this.getResolvedStageAndRegion();
     this.serverless.service.functions[this.proxyFunctionName] = {
-      handler: 'node_modules/serverless-mongo-proxy/dist/proxy.default',
+      handler: path.join(handlerFolder, 'proxy.default'),
       name: `${this.serverless.service.service}-${options.stage}-${this.proxyFunctionName}`,
+      events: [],
     };
   }
 
